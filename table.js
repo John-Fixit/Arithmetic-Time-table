96a97f35d442e23319e2a227b4a6f148dc1beb0f
Adeoye John Oluwakayode <Johnfixit293@gmail.com>
2022-03-10 22:16:40 +0100

    I started the arithmetic times table Project

---
+++ table.js
@@ -0,0 +1,47 @@
+function Multiply() {
+    var time = ''
+    var val1 = block.value;
+    var val2 = line.value;
+    time+='<table border="1">'
+    for (let j = 1; j <= val2; j++) {
+        time+='<tr>'
+            for (let i = 1; i <= val1; i++) {
+                time += '<td> ' + i + ' x ' + j + ' = ' + (i * j) + ' </td>'
+            }
+        '</tr>'
+    }
+'</table>'
+    document.getElementById('result').innerHTML = time
+}
+function Add(){
+    var val1 = block.value;
+    var val2 = line.value;
+    var time = ''
+    time+='<table border=1>'
+        for (j = 1; j<=val1; j++){
+       time+='<tr>'
+        for (i=1; i <= val2; i++){
+            time+='<td>' + j + '+' + i + '=' + (Number(j)+Number(i)) + '</td>'
+        }
+        '</tr>' 
+    }
+        '</table>'
+    document.getElementById('result').innerHTML = time
+
+}
+function Subtract(){
+    var val1 = block.value;
+    var val2 = line.value;
+    var time = ''
+    time+='<table border=1>'
+        for (j = 1; j<=val1; j++){
+       time+='<tr>'
+        for (i=1; i <= val2; i++){
+            time+='<td>' + j + '-' + i + '=' + (Number(j)-Number(i)) + '</td>'
+        }
+        '</tr>' 
+    }
+        '</table>'
+    document.getElementById('result').innerHTML = time
+
+}
