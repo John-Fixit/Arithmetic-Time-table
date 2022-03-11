b1a6b2249198d63de3f94a8ef273532d191a705b
Adeoye John Oluwakayode <Johnfixit293@gmail.com>
2022-03-11 14:50:42 +0100

    I added the selection button to it

--- table.js
+++ table.js
@@ -1,48 +1,32 @@
-function Multiply() {
-    var time = ''
-    var val1 = block.value;
-    var val2 = line.value;
-    time+='<table border="1">'
-    for (let j = 1; j <= val2; j++) {
-        time+='<tr>'
-            for (let i = 1; i <= val1; i++) {
-                time += '<td>' + i + ' x ' + j + ' = ' + (i * j) + ' </td>'
+
+function check(){
+    var choice = choose.options[choose.selectedIndex].value 
+    var val1 = block.value
+    var val2 = line.value
+    var total = ''
+    total+= '<table border=1>'
+        for (i = 1; i<=val1; i++){
+            total+='<tr>'
+            for (j = 1; j<=val2; j++){
+                if (choice==1) {
+                    total+='<td>'+ i + 'x' + j + '=' + (i*j)+
+                '</td>'
+                }
+               else if (choice==2) {
+                    total+='<td>'+ i + '+' + j + '=' + (Number(i)+ Number(j))+
+                '</td>'
+                }
+               else if (choice==3) {
+                   if (j<i) {
+                       total+='<td>' + i + '-' + j + (i-j)
+                   }
+                   else if (j>i) {
+                       total+='<td>' + j + '-' + i + '=' + (j-i) + '</td>'
+                   }
+                }
             }
-        '</tr>'
-    }
-'</table>'
-    document.getElementById('result').innerHTML = time
-}
-function Add(){
-    var val1 = block.value;
-    var val2 = line.value;
-    var time = ''
-    time+='<table border=1>'
-        for (j = 1; j<=val2; j++){
-       time+='<tr>'
-        for (i=1; i <= val1; i++){
-            time+='<td>' + j + '+' + i + '=' + (Number(j)+Number(i)) + '</td>'
+            '</tr>'
         }
-        '</tr>' 
-    }
-        '</table>'
-    document.getElementById('result').innerHTML = time
-
-}
-function Subtract(){
-    var val1 = block.value;
-    var val2 = line.value;
-    var time = ''
-    time+='<table border=1>'
-        for (j = 1; j<=val1; j++){
-       time+='<tr>'
-        for (i=1; i <= val2; i++){
-            time+='<td>' + j + '-' + i + '=' + (Number(j)-Number(i)) + '</td>'
-        }
-        '</tr>' 
-        time+='<br>'
-    }
-        '</table>'
-    document.getElementById('result').innerHTML = "multiplistiom 1 to " + j +time
-
+    '</table>'
+        result.innerHTML = total
 }
